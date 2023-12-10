73436346c07f60a44df12215beb49173d322dc86
hyunsu-im <dev.hyunsu@gmail.com>
2023-12-10 15:14:06 +0900

    Image 403 error fix

---
+++ README.md
@@ -0,0 +1,5 @@
+# webtoon
+A new Flutter project.
+
+# chorme 웹툰 이미지 불러올 때 에러 뜨는 경우 .vscode/launch.json 추가
+출처: https://softwarefactory-thinker.tistory.com/60
---
+++ lib/main.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/gestures.dart';
+import 'package:flutter/material.dart';
+import 'package:webtoon/screens/home_screen.dart';
+
+void main() {
+  runApp(const MainApp());
+}
+
+class MainApp extends StatelessWidget {
+  const MainApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      scrollBehavior: const MaterialScrollBehavior().copyWith(
+        dragDevices: {
+          PointerDeviceKind.mouse,
+          PointerDeviceKind.touch,
+          PointerDeviceKind.stylus,
+          PointerDeviceKind.unknown
+        },
+      ),
+      home: HomeScreen(),
+    );
+  }
+}
